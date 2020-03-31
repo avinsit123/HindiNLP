997f8845f42143fae13b57a89e817a6a278660ee
avinsit123 <r17935avinash@dpsrkp.net>
2020-03-31 11:31:03 +0530

    Modified setup.py

--- setup.py
+++ setup.py
@@ -24,8 +24,7 @@ setup(
            'typing',
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: NLP-Enthusiasts',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package     # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license     #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.6',
