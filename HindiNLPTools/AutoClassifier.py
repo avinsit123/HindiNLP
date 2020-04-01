b3a524956d0f2506b098fe062762fa8eb48abb36
avinsit123 <r17935avinash@dpsrkp.net>
2020-04-01 12:30:36 +0530

    Added Support to Download Checkpoint files from the Internet

--- HindiNLPTools/AutoClassifier.py
+++ HindiNLPTools/AutoClassifier.py
@@ -6,16 +6,16 @@ Created on Fri Mar 27 11:33:19 2020
 @author: r17935avinash
 """
 from flair.data import Corpus
-from flair.datasets import TREC_6
 from flair.embeddings import WordEmbeddings, DocumentRNNEmbeddings
 from flair.embeddings import FlairEmbeddings,StackedEmbeddings,TokenEmbeddings
 from flair.models import TextClassifier
 from flair.trainers import ModelTrainer
 from flair.datasets import CSVClassificationCorpus
-from typing import List
 from flair.visual.training_curves import Plotter
 from flair.data import Sentence,Dictionary
 import os
+import sys
+import torch
 
 class classifier():
     def __init__(self,data_folder):
@@ -26,42 +26,60 @@ class classifier():
                                          delimiter='\t' ) 
         self.label_dict = self.corpus.make_label_dictionary()
         self.embeddings = [FlairEmbeddings('hi-forward'),FlairEmbeddings('hi-backward'),]
+        self.download_path_split = torch.__file__.split("/")[:-2]
+        self.download_dir = "/".join(self.download_path_split) + "/HindiNLPTools"
+        self.dest_path = self.download_dir + "/resources/taggers/classifiers"
 
     def train(self,train_dict,dest_path= os.getcwd() + '/HindiNLPTools/resources/taggers/classifiers'):
+
+        if os.path.exists(dest_path) == False :
+            if not os.path.exists(self.download_dir + "/resources"):
+                os.mkdir(self.download_dir + "/resources")
+            if not os.path.exists(self.download_dir + "/resources/tagger"):
+                os.mkdir(self.download_dir + "/resources/tagger")
+            if not os.path.exists(self.download_dir + "/resources/tagger/classifiers"):
+                os.mkdir(self.download_dir + "/resources/tagger/classifiers")
+
         document_embeddings: DocumentRNNEmbeddings = DocumentRNNEmbeddings(self.embeddings,
                                                                      hidden_size=train_dict["hidden_size"],
                                                                      reproject_words=True,
                                                                      reproject_words_dimension=train_dict["output_size"])
         classifier = TextClassifier(document_embeddings, label_dictionary=self.label_dict)    
         trainer = ModelTrainer(classifier, self.corpus)
-        trainer.train(dest_path,
+        trainer.train(self.dest_path,
               learning_rate=train_dict["lr"],
               mini_batch_size=train_dict["batch_size"],
               max_epochs=train_dict["n_epochs"])
         
-    def Plot_Weights(self,dest_path=os.getcwd() + '/HindiNLPTools/resources/taggers/classifiers'):
+    def Plot_Weights(self):
+        if os.path.exists(dest_path) == False:
+            print("Error, First Train your models")
+            sys.exit()
         plotter = Plotter()
-        plotter.plot_weights(dest_path+"/weights.txt")
+        plotter.plot_weights(self.dest_path+"/weights.txt")
         
-    def Predict(input_text,dest_path= os.getcwd() + '/HindiNLPTools/resources/taggers/classifiers'):
-        classifier = TextClassifier.load(dest_path+"/final-model.pt")
+    def Predict(input_text):
+        if os.path.exists(dest_path) == False:
+            print("Error, First Train your models")
+            sys.exit()
+        classifier = TextClassifier.load(self.dest_path+"/final-model.pt")
         sentence = Sentence(input_text)
         classifier.predict(sentence)
         return sentence.labels
     
     
 
-#if __name__ == "__main__" :
-#    SVC = classifier("../Hindi-NLI/BBC")
-#    train_dict = {
-#            "hidden_size" : 512,
-#            "output_size" : 256,
-#            "lr" : 0.1 ,
-#            "batch_size" : 256 ,
-#            "n_epochs" : 150
-#            }
-#    SVC.train(train_dict)
-#    SVC.predict("I am a good man")
+if __name__ == "__main__" :
+   SVC = classifier("../../Desktop/Hindi-NLI/BBC")
+   train_dict = {
+           "hidden_size" : 512,
+           "output_size" : 256,
+           "lr" : 0.1 ,
+           "batch_size" : 256 ,
+           "n_epochs" : 150
+           }
+   SVC.train(train_dict)
+   SVC.predict("I am a good man")
 #    
 #        
 #        
--- HindiNLPTools/HindiNer.py
+++ HindiNLPTools/HindiNer.py
@@ -15,6 +15,7 @@ from flair.trainers import ModelTrainer
 import flair
 import torch
 from flair.visual.training_curves import Plotter
+from google_drive import  download_file_from_google_drive
 import os
 
 class NER():
@@ -23,10 +24,32 @@ class NER():
             self.embedding_types: List[TokenEmbeddings] = [FlairEmbeddings('hi-forward'),
                                                            FlairEmbeddings('hi-backward')]
             self.embeddings: StackedEmbeddings = StackedEmbeddings(embeddings=self.embedding_types)
-            
+            self.download_path_split = torch.__file__.split("/")[:-2]
+            self.download_dir = "/".join(self.download_path_split) + "/HindiNLPTools"
+            self.checkpoint_path = self.download_dir + "/resources/tagger/example-ner/best-model.pt"
+            self.checkpoint_download = not os.path.exists(self.download_dir+"/resources/tagger/example-ner/best-model.pt")
+            self.github_link = "https://github.com/avinsit123/HindiNLPTools/raw/master/HindiNLPTools/resources/tagger/example-ner/best-model.pt"
+            self.google_id = "1iKGBguWsvdJRospRSQfHnlmdHkawExJi"
+
         def Predict(self,text,is_path=False,path=""):
+           print(self.checkpoint_download)
            if is_path == False:
-               path = os.getcwd() + "/HindiNLPTools/resources/tagger/example-ner/best-model.pt"
+                if self.checkpoint_download == False:
+                    path = self.checkpoint_path
+                    print("Checkpoint File already present")
+                else:
+                    if not os.path.exists(self.download_dir + "/resources"):
+                        os.mkdir(self.download_dir + "/resources")
+                    if not os.path.exists(self.download_dir + "/resources/tagger"):
+                        os.mkdir(self.download_dir + "/resources/tagger")
+                    if not os.path.exists(self.download_dir + "/resources/tagger/example-ner"):
+                        os.mkdir(self.download_dir + "/resources/tagger/example-ner")
+                    print("Checkpoint File will be downloaded from ....")
+                    download_file_from_google_drive(self.google_id, self.checkpoint_path)
+                    print("Checkpoint Downloaded successfully")
+                    path = self.checkpoint_path
+                    self.checkpoint_download = False
+                    
            sentence = Sentence(text)
            tagger = SequenceTagger.load(path)
            tagger.predict(sentence)
@@ -34,9 +57,26 @@ class NER():
        
        
         def Predict_textfile(self,textfile,is_path=False,path=""):
+
             if is_path == False:
-                path = os.getcwd() + "/HindiNLPTools/resources/tagger/example-ner/best-model.pt"
+                if self.checkpoint_download == False:
+                    path = self.checkpoint_path
+                    print("Checkpoint File already present")
+                else:
+                    if not os.path.exists(self.download_dir + "/resources"):
+                        os.mkdir(self.download_dir + "/resources")
+                    if not os.path.exists(self.download_dir + "/resources/tagger"):
+                        os.mkdir(self.download_dir + "/resources/tagger")
+                    if not os.path.exists(self.download_dir + "/resources/tagger/example-ner"):
+                        os.mkdir(self.download_dir + "/resources/tagger/example-ner")
+                    print("Checkpoint File will be downloaded from ....")
+                    download_file_from_google_drive(self.google_id, self.checkpoint_path)
+                    print("Checkpoint Downloaded successfully")
+                    path = self.checkpoint_path
+                    self.checkpoint_download = False
+
             tagger = SequenceTagger.load(path)
+            
             dest_path = textfile[:-4]+"__NER.txt"
             out_f = open(dest_path,"w")
             with open(textfile,"r") as f:
@@ -63,7 +103,8 @@ class NER():
                                         tag_type=self.tag_type,
                                         use_crf=True)
             trainer: ModelTrainer = ModelTrainer(tagger, corpus)
-            trainer.train(os.getcwd() + '/HindiNLPTools/resources/taggers/saved-models',
+            
+            trainer.train( '/HindiNLPTools/resources/taggers/saved-models',
               learning_rate=train_dict["lr"],
               mini_batch_size=train_dict["batch_size"],
               max_epochs=train_dict["epochs"])
@@ -74,22 +115,22 @@ class NER():
          #  self.tagger = 
            
 #            
-#if __name__ == "__main__" :
-#    detect_ner = HindiNER()
-##    sentence = detect_ner.Predict("अविनाश आगरा में रहता है")
-##    print(sentence)
-##    detect_ner.Predict_textfile("Sentences.txt")
-#    train_dict = {
-#            "lr" : 0.1 ,
-#            "batch_size" : 32 ,
-#            "epochs" : 150 ,
-#            "hidden_size" : 256
-#            }
-#    detect_ner.train_NER(data_folder="/Users/r17935avinash/Desktop/Hindi-NLI/Ner",
-#                         train_file="train.txt",
-#                         dev_file="dev.txt",
-#                         test_file="test.txt",
-#                         train_dict=train_dict) 
+if __name__ == "__main__" :
+   detect_ner = NER()
+   sentence = detect_ner.Predict("अविनाश आगरा में रहता है")
+   print(sentence)
+#    detect_ner.Predict_textfile("Sentences.txt")
+   # train_dict = {
+   #         "lr" : 0.1 ,
+   #         "batch_size" : 32 ,
+   #         "epochs" : 150 ,
+   #         "hidden_size" : 256
+   #         }
+   #  detect_ner.train_NER(data_folder="/Users/r17935avinash/Desktop/Hindi-NLI/Ner",
+   #                       train_file="train.txt",
+   #                       dev_file="dev.txt",
+   #                       test_file="test.txt",
+   #                       train_dict=train_dict)
 #    
 #    
 #    
